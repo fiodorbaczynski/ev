7201b7aa23b1c9f6b8098d04e756368b11eb5db9
Fiodor Baczyński <fiodorbaczynski@gmail.com>
2021-12-11 19:12:40 +0100

    Basic setup

--- mix.exs
+++ mix.exs
@@ -6,23 +6,54 @@ defmodule Ev.MixProject do
       app: :ev,
       version: "0.1.0",
       elixir: "~> 1.12",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      dialyzer: [
+        plt_add_apps: [:mix, :ex_unit],
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  def application() do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp deps() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.6.1", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1.0", only: [:dev], runtime: false},
+      {:mox, "~> 1.0", only: :test},
+      {:jason, "~> 1.0"},
+      {:ecto_sql, "~> 3.4"}
     ]
   end
+
+  defp description() do
+    """
+    EV
+    """
+  end
+
+  defp package() do
+    [
+      files: ["lib", "priv", "mix.exs", "README.md"],
+      maintainers: ["Fiodor Baczyński"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/fiodorbaczynski/ev/",
+        "Docs" => "https://hexdocs.pm/ev/"
+      }
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+
+  defp elixirc_paths(_), do: ["lib"]
 end
